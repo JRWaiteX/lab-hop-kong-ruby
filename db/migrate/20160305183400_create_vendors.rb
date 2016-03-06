87d0d2806ee18a3133ae392b54bf62ca5bd1594a
Julien Waite <julienwaite@gmail.com>
2016-03-06 11:06:37 +0800

    add extra gems, user model and vendor model

---
+++ db/migrate/20160305183400_create_vendors.rb
@@ -0,0 +1,20 @@
+class CreateVendors < ActiveRecord::Migration
+  def change
+    create_table :vendors do |t|
+      t.string :name,       null: false
+      t.string :image_url,  null: false
+      t.string :street,     null: false
+      t.string :locality,   null: false
+      t.string :area,       null: false
+      t.string :type,       null: false
+      t.string :phone
+      t.string :email
+      t.string :website
+      t.string :facebook
+      t.string :twitter
+      t.string :instagram
+
+      t.timestamps null: false
+    end
+  end
+end
